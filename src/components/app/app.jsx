6d01a3000ef59181b496b122a032e4353d4c32b2
Maria Izvekova <yaforelka@gmail.com>
2020-06-10 09:18:01 +0300

    refactor according to comments

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,10 +1,8 @@
 import React from 'react';
 import WelcomeScreen from '../welcome-screen/welcome-screen.jsx';
 
-const App = (props) => {
-  // eslint-disable-next-line react/prop-types
-  const {errorCount} = props;
-
+// eslint-disable-next-line react/prop-types
+const App = ({errorCount}) => {
   return <WelcomeScreen errorCount={errorCount} />;
 };
 
